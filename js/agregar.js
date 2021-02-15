51e702ba3390d75f18e97f10c50889d4dc26db04
ney <fredneyparra0@gmail.com>
2021-02-14 20:55:38 -0400

    funcionando agregar con el input

--- js/agregar.js
+++ js/agregar.js
@@ -1,10 +1,10 @@
 const// Variables
-      input = document.getElementById('inputAgregar');
+      input = document.getElementById('inputAgregar'),
       formularioAgregar = document.getElementById('formularioAgregar'),
       itemList = document.getElementById('container-item-solo');
-
-    // Evento Agregar
-formularioAgregar.addEventListener('submit',(e) => {
+      
+      // Evento Agregar
+      formularioAgregar.addEventListener('submit',(e) => {
     e.preventDefault();
     // Crear elementos
     const divItem = document.createElement('div');
@@ -12,7 +12,7 @@ formularioAgregar.addEventListener('submit',(e) => {
     // div .container
     const agregarContainer = document.createElement('div');
     agregarContainer.className = 'agregar__container';
-    // label 
+    // label
     const agregarCheckbox = document.createElement('label');
     agregarCheckbox.className = 'agregar__checkbox';
     // input checkbox
@@ -24,9 +24,9 @@ formularioAgregar.addEventListener('submit',(e) => {
     agregarSpan.className = 'agregar__span';
     // text item
     const agregarLink = document.createElement('a');
-    agregarLink.className = 'agregar__link';
-    agregarLink.setAttribute('href','#');
-    // link icon 
+     agregarLink.className = 'agregar__link';
+     agregarLink.setAttribute('href','#');
+    // link icon
     const agregarLinkIcon = document.createElement('a');
     agregarLinkIcon.className = 'agregar__link-icon';
     agregarLinkIcon.setAttribute('href','#');
@@ -35,30 +35,60 @@ formularioAgregar.addEventListener('submit',(e) => {
     agregarIco.className = 'agregar__icon';
     agregarIco.setAttribute('src','images/icon-cross.svg');
     agregarIco.setAttribute('alt','clear');
-
+    
 divItem.appendChild(agregarContainer);
     agregarContainer.appendChild(agregarCheckbox);
-        agregarCheckbox.appendChild(agregarInput);
+    agregarCheckbox.appendChild(agregarInput);
         agregarCheckbox.appendChild(agregarSpan);
     agregarContainer.appendChild(agregarLink);
-        agregarLink.textContent = input.value;
+    agregarLink.textContent = input.value;
     divItem.appendChild(agregarLinkIcon);
-        agregarLinkIcon.appendChild(agregarIco);
+    agregarLinkIcon.appendChild(agregarIco);
 
 itemList.appendChild(divItem);
 
+const checkFirst = document.querySelector('.agregar__input--first');
+
+            if (checkFirst.checked === true) {
+                agregarLink.classList.toggle('agregar__link--decoration'); 
+                agregarInput.setAttribute('checked', true);  
+            }
+            
 input.value = '';
 });
 
+// line-thought
+const check = document.querySelectorAll('.agregar__input');
+        
+check.forEach(function (element) {
+    element.addEventListener('click', function () {
+        const ancestro = this.closest('.agregar__container');
+        const hijoAncestro = ancestro.querySelector('.agregar__link');
+        hijoAncestro.classList.toggle('agregar__link--decoration');
 
-const check = document.getElementById('check');
-const link = document.getElementById('agregar__link');
+        // determinar si esta checkeado o no
+// const checkFirst = document.querySelector('.agregar__input--first');
 
-check.addEventListener('click', function() {
-    if (this.checked == true) {
-        link.textContent = 'cambiado';
-        /*console.log(this.checked);*/
-      } else {
-        /*console.log(this.checked);*/
-      }
-});
+//         checkFirst.addEventListener('click', () => {
+//             if (checkFirst.checked === true) {
+//                 const padreAnterior = this.closest('.agregar__container');
+//                 padreAnterior.classList.toggle('agregar__link--decoration');
+//             }else if (checkFirst.checked === false) {
+//                 agregarLink.classList.toggle('agregar__link');
+//             }else {
+//                 console.log('no funciona');
+//             }
+//         });
+     });
+ });
+
+// eliminar elemento 
+// const del = document.querySelectorAll('.agregar__link-icon');
+
+// del.forEach(function (element) {
+//     element.addEventListener('click', function () {
+//         const delpadre = this.closest('.item');
+//         delpadre.remove();
+//     });
+// });
+        
