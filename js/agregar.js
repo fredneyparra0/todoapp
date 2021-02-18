b7a2b3359038159f6030185d8bd57c78e818e070
ney <fredneyparra0@gmail.com>
2021-02-18 15:06:00 -0400

    version estable- a falta de local storage

--- js/agregar.js
+++ js/agregar.js
@@ -1,113 +1,89 @@
 const// Variables
   input = document.getElementById('inputAgregar'),
   formularioAgregar = document.getElementById('formularioAgregar'),
-  itemList = document.getElementById('container-item-solo');
+  itemList = document.getElementById('container-item-solo'),
+  checkFirst = document.querySelector('.agregar__input--first');
 
 formularioAgregar.addEventListener('submit', (e) => {
   e.preventDefault();
+  const item = document.createElement('div');
+  itemList.appendChild(createElementWithEvents(item));
+  input.value = '';
+  });
+  
+const createElementWithEvents = (item) => {
+  item.className = 'item';
+  item.innerHTML = `
+    <div class="agregar__container">
+      <label class="agregar__checkbox">
+        <input class="agregar__input" type="checkbox">
+        <span class="agregar__span"></span>
+        </input>
+      </label>
+      <a class="agregar__link" href="#">${input.value}</a>
+    </div>
+    <a class="agregar__link-icon" href="#">
+      <img class="agregar__link-icon" src="images/icon-cross.svg" alt="clear">
+    </a>
+  `;
+  const inputItem = item.querySelector('input.agregar__input'),
+        link = item.querySelector('a.agregar__link'),
+        deleteItem = item.querySelector('a.agregar__link-icon');
 
-  const elementHtml = () => {
-    // item
-    const item = document.createElement('div');
-    item.className = 'item';
-
-    item.innerHTML = `
-      <div class="agregar__container">
-        <label class="agregar__checkbox">
-          <input class="agregar__input" type="checkbox">
-          <span class="agregar__span"></span>
-          </input>
-        </label>
-        <a class="agregar__link" href="#">${input.value}</a>
-      </div>
-      <a class="agregar__link-icon" href="#">
-        <img class="agregar__link-icon" src="images/icon-cross.svg" alt="clear">
-      </a>
-    `;
-
-    console.log(item);
-
-    itemList.appendChild(item);
-
-
-    // container--label--input--span--a
-    // const divContainer = document.createElement('div');
-    // divContainer.className = 'agregar__container';
-
-    // const labelAgregar = document.createElement('label');
-    // labelAgregar.className = 'agregar__checkbox';
-
-    // const inputLabel = document.createElement('input');
-    // inputLabel.className = 'agregar__input';
-    // inputLabel.setAttribute('type','checkbox');
-
-    // const spanLabel = document.createElement('span');
-    // spanLabel.className = 'agregar__span';
-
-    // const aAgregar = document.createElement('a');
-    // aAgregar.className = 'agregar__link';
-    // aAgregar.textContent = input.value;
-    // aAgregar.setAttribute('href','#');
-
-    // labelAgregar.appendChild(inputLabel);
-    // labelAgregar.appendChild(spanLabel);
-    // divContainer.appendChild(labelAgregar);
-    // divContainer.appendChild(aAgregar);
-
-    // // a--img
-    // const aIcon = document.createElement('a');
-    // aIcon.className = 'agregar__link-icon';
-    // aIcon.setAttribute('href','#');
-
-    // const imgIcon = document.createElement('img');
-    // imgIcon.className = 'agregar__icon';
-    // imgIcon.setAttribute('src','images/icon-cross.svg');
-
-    // aIcon.appendChild(imgIcon);
-
-    // item.appendChild(divContainer);
-    // item.appendChild(aIcon);
-
-    // itemList.appendChild(item);
-
-    //check, nocheck
-    const checkFirst = document.querySelector('.agregar__input--first');
-
-    if (checkFirst.checked === true) {
-      aAgregar.classList.toggle('agregar__link--decoration');
-      inputLabel.setAttribute('checked', true);
-    }
-
-  };
-  elementHtml();
-
-  const del = document.querySelectorAll('.agregar__link-icon');
-
-  del.forEach(function (element) {
-    element.addEventListener('click', function () {
-      const delpadre = this.closest('.item');
-      delpadre.remove();
-    });
+  deleteItem.addEventListener('click', () => {
+     const ancestroDel = deleteItem.closest('.item');
+    ancestroDel.remove();
   });
 
-  const check = document.querySelectorAll('.agregar__input');
 
- check.forEach(function (element) {
-   element.addEventListener('click', function () {
-     const ancestro = this.closest('.agregar__container');
-     const hijoAncestro = ancestro.querySelector('.agregar__link');
-     hijoAncestro.classList.toggle('agregar__link--decoration');
-   });
- });
+  inputItem.addEventListener('click', function () {
+    link.classList.toggle('agregar__link--decoration');
+  });      
+  
+  if (checkFirst.checked) {
+    link.classList.add('agregar__link--decoration');
+    inputItem.setAttribute('checked', true);
+  }
+  return item;
+};
 
-  input.value = '';
+const clearAll = document.querySelector('span#clearAll');
+
+clearAll.addEventListener('click', function () {
+  const items = this.closest('.items-list-all');
+  const hijoItem = items.querySelector('#container-item-solo');
+  const itemDelAll = hijoItem.querySelectorAll('div.item');
+  itemDelAll.forEach(element => {
+    element.remove();
+  });
 });
 
+  //  elementHtml();
+
+//   const del = document.querySelectorAll('.agregar__link-icon');
+
+//   del.forEach(function (element) {
+//     element.addEventListener('click', function () {
+//       const delpadre = this.closest('.item');
+//       delpadre.remove();
+//     });
+//   });
+
+//   const check = document.querySelectorAll('.agregar__input');
+
+//  check.forEach(function (element) {
+//    element.addEventListener('click', function () {
+//      const ancestro = this.closest('.agregar__container');
+//      const hijoAncestro = ancestro.querySelector('.agregar__link');
+//      hijoAncestro.classList.toggle('agregar__link--decoration');
+//    });
+//  });
 
 
-//check, nocheck
-const checkBox = document.querySelector('.agregar__input');
-const cambiarTitle = document.querySelectorAll('agregar__link');
+
+// //check, nocheck
+// const checkBox = document.querySelector('.agregar__input');
+// const cambiarTitle = document.querySelectorAll('agregar__link');
 
 // console.log(cambiarTitle);
 
