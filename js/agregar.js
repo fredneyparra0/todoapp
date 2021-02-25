85e62bd886db6f92be2412fa9ae06333e5d14cf8
ney <fredneyparra0@gmail.com>
2021-02-25 18:23:01 -0400

    refactorizacion de funciones, solo falta bug al recargar la pagina

--- js/agregar.js
+++ js/agregar.js
@@ -2,53 +2,97 @@ const// Variables
   input = document.getElementById('inputAgregar'),
   formularioAgregar = document.getElementById('formularioAgregar'),
   itemList = document.getElementById('container-item-solo'),
-  checkFirst = document.querySelector('.agregar__input--first');
+  checkFirst = document.querySelector('.agregar__input--first'),
+  clearAll = document.querySelector('span#clearAll');
 
+// Agregar elemento desde input
 formularioAgregar.addEventListener('submit', (e) => {
   e.preventDefault();
-  const item = document.createElement('div');
-  itemList.appendChild(createElementWithEvents(item));
+  itemList.appendChild( crearElementoDiv(input.value) );
+  const arrayObject = guardarLocalStorage( guardarValoresLocalStorage(input.value,checkFirst.checked) );
+  convertirJSON(arrayObject);
+
+  // cambiarCheck();
   input.value = '';
-  });
-  
-const createElementWithEvents = (item) => {
-  item.className = 'item';
-  item.innerHTML = `
+});
+// Agregar elementos que se encuentran el localStorage
+const cargaRapida = addEventListener('DOMContentLoaded', () => {
+  const valorQueViene = recibirValoresLocalStorage();
+  const fragmentLocalStorage = document.createDocumentFragment();
+  if(valorQueViene === null){
+ 
+  }else {
+    valorQueViene.forEach((element) => {
+      const divFuncion = crearElementoDiv(element.titulo);
+      fragmentLocalStorage.appendChild(divFuncion);
+    });
+  }
+  itemList.appendChild(fragmentLocalStorage);
+});
+//Crear Elemento HTML
+const crearElementoDiv = function ( inputText ) {
+  const elementoDiv = document.createElement('div');
+  elementoDiv.className = 'item';
+  elementoDiv.innerHTML = `
     <div class="agregar__container">
       <label class="agregar__checkbox">
         <input class="agregar__input" type="checkbox">
         <span class="agregar__span"></span>
         </input>
       </label>
-      <a class="agregar__link" href="#">${input.value}</a>
+      <a class="agregar__link" href="#">${inputText}</a>
     </div>
     <a class="agregar__link-icon" href="#">
       <img class="agregar__link-icon" src="images/icon-cross.svg" alt="clear">
     </a>
   `;
-  const inputItem = item.querySelector('input.agregar__input'),
-        link = item.querySelector('a.agregar__link'),
-        deleteItem = item.querySelector('a.agregar__link-icon');
-
-  deleteItem.addEventListener('click', () => {
-     const ancestroDel = deleteItem.closest('.item');
-    ancestroDel.remove();
-  });
-
-
-  inputItem.addEventListener('click', function () {
-    link.classList.toggle('agregar__link--decoration');
-  });      
-  
+  // Variables elemento div.item
+  const decoracionTexto = elementoDiv.querySelector('.agregar__link'),
+        check = elementoDiv.querySelector('.agregar__input');
+  // Agregar check o noCheck
   if (checkFirst.checked) {
-    link.classList.add('agregar__link--decoration');
-    inputItem.setAttribute('checked', true);
+    decoracionTexto.classList.add('agregar__link--decoration');
+    check.setAttribute('checked', true);
   }
-  return item;
-};
-
-const clearAll = document.querySelector('span#clearAll');
-
+  // Agregar lineTought
+  check.addEventListener('click', function () {
+    decoracionTexto.classList.toggle('agregar__link--decoration');
+  });
+  return elementoDiv;
+}
+// Guardar valores en objeto
+function guardarValoresLocalStorage (text,check){
+  const objetoInsertar = {
+    titulo : text,
+    check : check
+  }
+  return objetoInsertar;
+}
+// Guardar objeto en array
+function guardarLocalStorage (objeto) {
+  objetolocalStorage.push(objeto);
+  return objetolocalStorage;
+}
+const objetolocalStorage = [];
+// Convertir array a json
+function convertirJSON (array) {
+  const arregloJson = JSON.stringify(array);
+  localStorage.setItem('items',arregloJson);
+}
+// Cambiar local storage check
+// function cambiarCheck () {
+//   const checkLista = document.querySelectorAll('input.agregar__input');
+//   checkLista.addEventListener('click', function () {
+    
+//   });
+// }
+// Reconvertir de JSON a objeto
+const recibirValoresLocalStorage = () => {
+  const valor = localStorage.getItem('items');
+  const parse = JSON.parse(valor);
+  return parse;
+}
+// Eliminar todos los elementos de la lista // FALTA VOLVER MENOS LARGO
 clearAll.addEventListener('click', function () {
   const items = this.closest('.items-list-all');
   const hijoItem = items.querySelector('#container-item-solo');
@@ -57,66 +101,3 @@ clearAll.addEventListener('click', function () {
     element.remove();
   });
 });
-
-  //  elementHtml();
-
-//   const del = document.querySelectorAll('.agregar__link-icon');
-
-//   del.forEach(function (element) {
-//     element.addEventListener('click', function () {
-//       const delpadre = this.closest('.item');
-//       delpadre.remove();
-//     });
-//   });
-
-//   const check = document.querySelectorAll('.agregar__input');
-
-//  check.forEach(function (element) {
-//    element.addEventListener('click', function () {
-//      const ancestro = this.closest('.agregar__container');
-//      const hijoAncestro = ancestro.querySelector('.agregar__link');
-//      hijoAncestro.classList.toggle('agregar__link--decoration');
-//    });
-//  });
-
-
-
-// //check, nocheck
-// const checkBox = document.querySelector('.agregar__input');
-// const cambiarTitle = document.querySelectorAll('agregar__link');
-
-// console.log(cambiarTitle);
-
-
-/*if (checkBox.checked === true) {
-  aAgregar.classList.toggle('agregar__link--decoration');
-  inputLabel.setAttribute('checked', true);
-}*/
-// //  eliminar elemento ////// DE MOMENTO NO ES NECESARIO
-//  const del = document.querySelectorAll('.agregar__link-icon');
-
-//  del.forEach(function (element) {
-//      element.addEventListener('click', function () {
-//          const delpadre = this.closest('.item');
-//          delpadre.remove();
-//      });
-//  });
-
-
-// const persona = {
-//   nombre: 'Federico',
-//   apellido: 'Martinez',
-//   edad: 21
-// }
-
-// console.log(persona);
-
-// const textoJson = JSON.stringify(persona);
-
-// console.log(textoJson);
-
-// localStorage.setItem('person' ,textoJson);
-
-// persona1 = JSON.parse(textoJson);
-
-// console.log(persona1);
